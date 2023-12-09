ef02919e1016307fd523b421cb6bdf965a8e4b82
bayudsatriyo <bayudsatriyo@gmail.com>
2023-12-09 15:50:53 +0700

    change maxbytes 5mb

--- src/api/pesanan/routes.js
+++ src/api/pesanan/routes.js
@@ -8,7 +8,7 @@ const routes = (handler) => [
         allow: 'multipart/form-data',
         multipart: true,
         output: 'stream',
-        maxBytes: 512000, // 512KB
+        maxBytes: 5242880, // 512KB
       },
     },
   },
